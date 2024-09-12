bd28320c23d7db14e358191e1f84e4c0f3fa9adb
Alexsandro <alessandro85521239@gmail.com>
2024-09-12 15:02:06 -0300

    criado json para salvar

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const { select, input, checkbox } = require ('@inquirer/prompts')
+const fs = require("fs").promises
 
-let mensagem = "Bem vindo ao app de metas";
+let mensagem = "Bem vindo ao app de metas"
 
-let meta = {
-    value: 'Tomar 3L de agua por dia',
-    checked: false,
+
+
+
+let metas
+
+const carregarMetas = async () => {
+    try{
+       const dados = await fs.readFile("metas.json", "utf-8")
+       metas = JSON.parse(dados)
+    }
+    catch(erro) {
+        metas = []
+    }
 
 }
 
-let metas = [ meta]
+const salvarMetas = async () => {
+    await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+
+}
+
 
 const cadastrarMeta = async () => {
     const meta = await input({ message: "Digite a meta:"})
@@ -127,9 +142,13 @@ const mostrarMensagem = () => {
 }
 
 const start = async () => {
-   
-   while(true) {
-    mostrarMensagem()    
+    await carregarMetas()
+
+    while(true) {
+    mostrarMensagem()  
+    await salvarMetas()
+    
+    
     const opcao = await select  ({
         message: "Menu >",
         choices: [
@@ -164,7 +183,7 @@ const start = async () => {
 
         switch(opcao) {
             case "cadastrar":
-                await cadastrarMeta()               
+                await cadastrarMeta()           
                 break
             case "listar":
                 await listarMetas()
