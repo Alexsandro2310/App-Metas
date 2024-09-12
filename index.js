f6ab4507f0410bb5526bf464df65e43bd2d979bc
Alexsandro <alessandro85521239@gmail.com>
2024-09-12 08:58:49 -0300

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -48,6 +48,26 @@ const listarMetas = async () => {
     console.log('Metas marcadas como concluidas')
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+    if(realizadas.length == 0) {
+        console.log('Não existem metas realizadas! :/')
+    return
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+
+    })
+    
+}
+
+
+
+
 const start = async () => {
    
    while(true) {
@@ -62,6 +82,10 @@ const start = async () => {
                 {
                     name: "Listar Metas",
                     value: "listar"
+                },
+                {
+                    name: "Metas realizadas",
+                    value: "realizadas"
                 },
                 {
                     name: "sair",
@@ -79,6 +103,9 @@ const start = async () => {
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a proxima!")
